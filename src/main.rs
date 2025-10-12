013cf8eb73d800179af49dd2ac6fb9cadd363163
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2025-10-12 19:52:51 +0200

    chore: better logging

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,19 @@ use std::sync::Arc;
 
 use fuser::MountOption;
 
+use tracing::{debug, error, warn, info};
+
 fn main() -> anyhow::Result<()> { 
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
-        eprintln!("Usage: {} <server_url> <mountpoint>", args[0]);
+        info!("Usage: {} <server_url> <mountpoint>", args[0]);
         std::process::exit(1);
     }
 
     let server_url = &args[1];
     let mountpoint = &args[2];
 
-    println!("Mounting remote filesystem from {} to {}", server_url, mountpoint);
+    debug!("Mounting remote filesystem from {} to {}", server_url, mountpoint);
 
     let http_client =  Arc::new(HttpClient::new(server_url.to_string()));
     let fs = RemoteFileSystem::new(http_client.clone());
